8d3737c031fd655582c2bbeaa4d37bf1c4d5ddea
nirmalya <nirmalya@broadinstitute.org>
2017-04-11 12:14:49 -0400

    Reading and processing barcodes

--- barcode_loader.cpp
+++ barcode_loader.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <unordered_map>
 #include <fstream>
 
 #include <boost/regex.hpp>
@@ -11,19 +11,43 @@
 class BCLoader {
 
     public:
+        BCLoader() = default ;
         BCLoader(std::string bc_file, bool hasHeader = true);
+
+        // Copy constructor
+        BCLoader(const BCLoader& bcl) {
+            bc_file = bcl.bc_file;
+            hasHeader = bcl.hasHeader;
+        }
+
         bool load_map();
-        bool load_tree();
         void save_tree(std::string save_file);
+        int get_index(std::string seq);
+        void print_map();
+        void print_index();
+        bool load_tree();
+        std::string val_from_bc_map(std::string lbc);
+        std::vector<std::string> vals_from_tree(std::string barcode, int mm);
 
     private:
         bool hasHeader;
-        std::map<std::string, std::string> bc_map;
-        std::map<std::string, std::string> rev_bc_map;
+        std::unordered_map<std::string, std::string> bc_map;
+        std::unordered_map<std::string, std::string> rev_bc_map;
+        std::unordered_map<std::string, int> seq_to_ind;
         std::string bc_file;
         BKTree<std::string> tree;
 
 };
+
+std::string BCLoader::val_from_bc_map(std::string lbc) {
+    std::string val = bc_map[lbc];
+    return val;
+}
+
+std::vector<std::string> BCLoader::vals_from_tree(std::string barcode, int mm) {
+    std::vector<std::string> vals = tree.find(barcode, mm);
+    return vals;
+}
 
 BCLoader::BCLoader (std::string bc_file, bool hasHeader):
     bc_file(bc_file),
@@ -51,7 +75,10 @@ void BCLoader::save_tree(std::string outfile) {
     return;
 }
 
-
+int BCLoader::get_index(std::string seq) {
+    int l_index = seq_to_ind[seq];
+    return l_index;
+}
 
 bool BCLoader::load_map() {
     std::ifstream words(bc_file);
@@ -69,30 +96,53 @@ bool BCLoader::load_map() {
             std::cout << "Ignored the header. " << "\n";
             std::getline(words, lstr);
         }
+        int l_index = 0;
         while (std::getline(words, lstr)) {
             bool res = boost::regex_search(lstr, what, expr);
             if (res) {
                 std::string bc_name = what[1];
                 std::string  bc_val = what[2];
-                bc_map.insert(std::pair<std::string, std::string>(bc_name, bc_val));
+                bc_map[bc_name] = bc_val;
+                seq_to_ind[bc_val] = l_index; 
+                l_index++;
             } else {
                 throw my_exception("Problem in the parsing the barcode lines.\n");
             }
         }
     }
-    
-    std::cout << "Printing forward bc map" << "\n";
+    return true; 
+}
+
+void BCLoader::print_map() {
+     
+    std::cout << "Printing forward bc map for : " << bc_file << "\n";
+    std::cout << "------------------------------\n";
     for (const auto& val_pair : bc_map) {
         auto & bc_name = val_pair.first;
         auto & bc_val = val_pair.second;
         int bc_len = bc_val.length();
-        //std::cout << "barcode: " << bc_name << " val : "  << bc_val << " bc_len: " << bc_len << "\n";
+        std::cout << "barcode: " << bc_name << " val : "  << bc_val << " bc_len: " << bc_len << "\n";
     
     }
-    return true; 
+    std::cout << "...............................\n";
     
 }
 
+
+void BCLoader::print_index() {
+    std::cout << "Printing bc index for : " << bc_file << "\n";
+    std::cout << "------------------------------\n";
+    for (const auto& val_pair : seq_to_ind) {
+        auto & bc_name = val_pair.first;
+        auto & bc_ind = val_pair.second;
+        std::cout << "barcode: " << bc_name << " index : "  << bc_ind << "\n";
+    
+    }
+    std::cout << "...............................\n";
+
+}
+
+/*
 int main(int argc, char* argv[]) {
     std::string i5_to_stagger_file = argv[1];
 
@@ -117,5 +167,6 @@ int main(int argc, char* argv[]) {
     bc_loader.save_tree(outfile);
 }
 
+*/
 
 
--- concensus_mono.cpp
+++ concensus_mono.cpp
@@ -7,11 +7,14 @@
 #include <cstring>
 #include <set>
 #include <cctype>
+#include <sstream>
+#include <algorithm>
 #include <memory>
 
 #include "BKTree.h"
 #include "fastq_reader.hpp"
 #include "fastq_writer.hpp"
+#include "barcode_loader.cpp"
 
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
@@ -31,6 +34,8 @@ class concensus_mono {
         void initialize();
         void core_engine();
         void print_help();
+        void load_barcodes();
+        std::string vec_to_str(std::vector<std::string> my_vector);
     private:
 		// Input variables pushed through command line
 		std::string i5_file_str;
@@ -46,6 +51,11 @@ class concensus_mono {
 		int mm_sbc;
         po::options_description desc;
 
+        BCLoader i5_loader;
+        BCLoader i7_loader;
+        BCLoader sbc_loader;
+        BCLoader stagger_loader;
+
 };
 
 
@@ -56,6 +66,34 @@ void concensus_mono::print_help() {
         " -p <prefix_str> -o <outdir>\n\n";
 }
 
+
+void concensus_mono::load_barcodes() {
+    i5_loader = BCLoader(i5_file_str);
+    i5_loader.load_map();
+    i5_loader.load_tree();
+    i5_loader.print_map();
+    i5_loader.print_index();
+
+    i7_loader = BCLoader(i7_file_str);
+    i7_loader.load_map();
+    i7_loader.load_tree();
+    i7_loader.print_map();
+    i7_loader.print_index();
+
+    sbc_loader = BCLoader(sbc_file_str);
+    sbc_loader.load_map();
+    sbc_loader.load_tree();    
+    sbc_loader.print_map();
+    sbc_loader.print_index();
+
+    stagger_loader = BCLoader(stagger_file_str);
+    stagger_loader.load_map();
+    stagger_loader.print_map();
+
+}
+
+
+
 bool concensus_mono::parse_args(int argc, char* argv[]) {
     
     bool all_set = true;
@@ -63,17 +101,17 @@ bool concensus_mono::parse_args(int argc, char* argv[]) {
 
     desc.add_options()
 		("help,h", "produce help message")
-		("i5", po::value<std::string>(&i5_file_str), "index5 barcode file")
-		("i7", po::value<std::string>(&i7_file_str), "index7 barcode file")
-		("sbc", po::value<std::string>(&sbc_file_str), "strain barcode file")
-		("stagger", po::value<std::string>(&stagger_file_str), "i5 to stagger map file")
-		("short_read", po::value<std::string>(&short_read_str), "First file")
-		("long_read", po::value<std::string>(&long_read_str), "Second file")
-		("prefix,p", po::value<std::string>(&prefix_str), "Prefix string")
-		("outdir,o", po::value<std::string>(&outdirpath), "Output directory")	
+		("i5", po::value<std::string>(&i5_file_str)->required(), "index5 barcode file")
+		("i7", po::value<std::string>(&i7_file_str)->required(), "index7 barcode file")
+		("sbc", po::value<std::string>(&sbc_file_str)->required(), "strain barcode file")
+		("stagger", po::value<std::string>(&stagger_file_str)->required(), "i5 to stagger map file")
+		("short_read", po::value<std::string>(&short_read_str)->required(), "First file")
+		("long_read", po::value<std::string>(&long_read_str)->required(), "Second file")
+		("prefix,p", po::value<std::string>(&prefix_str)->required(), "Prefix string")
+		("outdir,o", po::value<std::string>(&outdirpath)->required(), "Output directory")	
 		("mm_i5", po::value(&mm_i5)->default_value(1), "Optional/Maximum allowed mismatches for i5 barcode.")
 		("mm_i7", po::value(&mm_i7)->default_value(1), "Optional/Maximum allowed mismatches for i7 barcode.")
-		("mm_sbc", po::value(&mm_sbc)->default_value(2), "Optional/Maximum allowed mismatches for sbc barcode.")
+		("mm_sbc", po::value(&mm_sbc)->default_value(3), "Optional/Maximum allowed mismatches for sbc barcode.")
 	;
 
     po::variables_map vm;
@@ -152,7 +190,31 @@ void concensus_mono::initialize() {
 	}
 }
 
+
+std::string concensus_mono::vec_to_str(std::vector<std::string> lvec) {
+    std::string res_str = "";
+    for (int j = 0; j < lvec.size(); j++) {
+        if (j == 0) {
+            res_str += lvec[j];
+        } else {
+            res_str += ", " + lvec[j];
+        }
+    }
+    return res_str;
+    
+}
+
+
+
+
 void concensus_mono::core_engine() {
+
+    // These are the information we may extract from config
+    // in the future
+    int i7_len = 8;
+    int i5_len = 8;
+    int gap_after_stagger = 18;
+    int sbc_len = 20;
 
     // The words starting with lword is for short read
 	std::string short_word1;
@@ -165,6 +227,11 @@ void concensus_mono::core_engine() {
 	std::string long_word2;
 	std::string long_word3;
 	std::string long_word4;
+
+    std::string i5_local;
+    std::string i7_local;
+    std::string sbc_local;
+    std::string stagger_local;
 
 
     fastq_reader short_file(short_read_str);
@@ -183,6 +250,29 @@ void concensus_mono::core_engine() {
         if (!long_file.getline(long_word3)) { break; }
         if (!long_file.getline(long_word4)) { break; }
         ++read_count;
+
+        // The entire short read consists i7_index
+        i7_local = short_word2.substr(0, i7_len);
+        i5_local = long_word2.substr(0, i5_len);
+        stagger_local = stagger_loader.val_from_bc_map(i5_local);
+        int stagger_len = stagger_local.length();
+        int sbc_start = i5_len + stagger_len + gap_after_stagger;
+        sbc_local = long_word2.substr(sbc_start, sbc_len);
+
+        const std::vector<std::string>& i5_vals = i5_loader.vals_from_tree(i5_local, mm_i5);
+        const std::vector<std::string>& i7_vals = i7_loader.vals_from_tree(i7_local, mm_i7);
+        const std::vector<std::string>& sbc_vals = sbc_loader.vals_from_tree(sbc_local, mm_sbc);
+        
+        std::cout << "i7_local: " << i7_local << " i5_local: " << i5_local << 
+            " stagger_local: " << stagger_local << " sbc_local: " << sbc_local << 
+            " i5_vals: " << vec_to_str(i5_vals) << " i7_vals: " << vec_to_str(i7_vals) << 
+            " sbc_vals: " << vec_to_str(sbc_vals) << "\n";
+        std::cout << "\n";
+
+       // Get the stagger sequence using i5
+       
+  
+        
         if (read_count % 1000000 == 0) {
             std::cout << "read_count: " << read_count << "\n";
         }
@@ -215,6 +305,7 @@ int main(int argc, char* argv[]) {
 
 	csm.initialize();
 	try {
+        csm.load_barcodes();
 		csm.core_engine();
 	} catch(std::invalid_argument& e) {
         std::cerr << "error: " << e.what() << "\n";
