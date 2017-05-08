0b8d549bc32b8fa3337bd687da83400981233137
nirmalya <nirmalya@broadinstitute.org>
2017-05-08 18:11:59 -0400

    Code seems to be working

--- combine_lanes.cpp
+++ combine_lanes.cpp
@@ -23,11 +23,13 @@ class combine_lanes {
         std::vector<std::string> split(const std::string &s, char delim); 
         std::string get_mapped_str(std::string key_str);
         void load_maps();
-        std::vector<std::string> combine_vectors(std::vector<std::string> vec1, 
-            std::vector<std::string> vec2, std::string delim);
+        template <typename T1, typename T2>
+        std::vector<std::string> combine_vectors(std::vector<T1> vec1, 
+            std::vector<T2> vec2, std::string delim);
         std::map<std::string, std::string> make_map(std::vector<std::string> vec1, 
             std::vector<std::string> vec2);
-        std::vector<std::string> change_to_string(std::vector<int> vec);
+        template <typename T>
+            std::vector<std::string> change_to_string(std::vector<T> vec);
         template <typename T1, typename T2>
             void print_map(std::map<T1, T2> lmap);
         void print_some_maps();
@@ -49,6 +51,7 @@ class combine_lanes {
         std::map<std::string, int> i5_key_to_quadrant;
         std::map<std::string, std::string> i7_map;
         std::map<std::string, std::string> plate_bcs_map;
+        std::map<std::string, std::string> compound_map;
 
 };
 
@@ -79,10 +82,13 @@ void combine_lanes::print_some_maps() {
     print_map(i7_map);
     std::cout << "Printing plate_bcs_map.\n";
     print_map(plate_bcs_map);
+    //std::cout << "Printing comp_map.\n";
+    //print_map(compound_map);
 
 }
 
-std::vector<std::string> combine_lanes::change_to_string(std::vector<int> vec) {
+template<typename T> 
+std::vector<std::string> combine_lanes::change_to_string(std::vector<T> vec) {
     std::vector<std::string> lvec;
     for (int j = 0; j < vec.size(); j++) {
         lvec.push_back(std::to_string(vec[j]));
@@ -90,14 +96,22 @@ std::vector<std::string> combine_lanes::change_to_string(std::vector<int> vec) {
     return lvec;
 }
 
+template<>
+std::vector<std::string> combine_lanes::change_to_string<std::string>(std::vector<std::string> vec) {
+     return vec;
+}
 
+template <typename T1, typename T2>
 std::vector<std::string> combine_lanes::combine_vectors
-            (std::vector<std::string> vec1, 
-            std::vector<std::string> vec2, std::string delim) {
+            (std::vector<T1> vec1, 
+            std::vector<T2> vec2, std::string delim) {
+    std::vector<std::string> vec1_sv = change_to_string<T1>(vec1);
+    std::vector<std::string> vec2_sv = change_to_string<T2>(vec2);
+
     std::vector<std::string> comb_vec;
     for (size_t j = 0; j < vec1.size(); j++) {
-        std::string var1 = vec1[j];
-        std::string var2 = vec2[j];
+        std::string var1 = vec1_sv[j];
+        std::string var2 = vec2_sv[j];
         std::string final_var = var1 + delim + var2;
         comb_vec.push_back(final_var);
     }
@@ -107,7 +121,7 @@ std::vector<std::string> combine_lanes::combine_vectors
 std::map<std::string, std::string> combine_lanes::make_map(std::vector<std::string> vec1, 
         std::vector<std::string> vec2) {
     std::map<std::string, std::string> lmap;
-    assert(vec1.size() == vec2.size());
+    //assert(vec1.size() == vec2.size());
     for (size_t i = 0; i < vec1.size(); ++i)
         lmap[vec1[i]] = vec2[i];
     return lmap;
@@ -125,10 +139,8 @@ void combine_lanes::load_maps() {
     std::vector<int> i7_quadrant = i7_plates_map.getColumn<int>("quadrant");
     std::vector<std::string> i7_row = i7_plates_map.getColumn<std::string>("row");
     std::vector<int> i7_column = i7_plates_map.getColumn<int>("column");
-    std::vector<std::string> i7_quadrant_sv = change_to_string(i7_quadrant);
-    std::vector<std::string> i7_column_sv = change_to_string(i7_column); 
-    std::vector<std::string> i7_key_quadrant = combine_vectors(i7_key, i7_quadrant_sv, "_");
-    std::vector<std::string> i7_row_column = combine_vectors(i7_row, i7_column_sv, "");
+    std::vector<std::string> i7_key_quadrant = combine_vectors<std::string, int>(i7_key, i7_quadrant, "_");
+    std::vector<std::string> i7_row_column = combine_vectors<std::string, int>(i7_row, i7_column, "");
     i7_map = make_map(i7_key_quadrant, i7_row_column);    
 
     std::string cdelim = "comma";
@@ -136,13 +148,25 @@ void combine_lanes::load_maps() {
     std::vector<std::string> plate_name = plate_bcs.getColumn<std::string>("plate_name");
     std::vector<int> primer_plate = plate_bcs.getColumn<int>("primer_plate");
     std::vector<int> pool = plate_bcs.getColumn<int>("pool");
-    std::vector<std::string> primer_plate_sv = change_to_string(primer_plate);
-    std::vector<std::string> pool_sv = change_to_string(pool);
-    std::vector<std::string> primer_plate_pool = combine_vectors(primer_plate_sv, pool_sv, "_");
+    std::vector<std::string> primer_plate_pool = combine_vectors<int, int>(primer_plate, pool, "_");
     plate_bcs_map = make_map(primer_plate_pool, plate_name);
-    
 
-    
+
+
+    MapLoader comp_map(comp_map_str, cdelim);
+    std::vector<std::string> Plate_Barcode = comp_map.getColumn<std::string>("Plate_Barcode");
+    std::vector<std::string> Well_Position = comp_map.getColumn<std::string>("Well_Position");
+    std::vector<std::string> Broad_Sample = comp_map.getColumn<std::string>("Broad_Sample");
+    std::vector<std::string> mg_per_ml = comp_map.getColumn<std::string>("mg_per_ml");
+    std::vector<std::string> mmoles_per_liter = comp_map.getColumn<std::string>("mmoles_per_liter");
+
+    std::vector<std::string> Plate_Barcode_Well_Position = combine_vectors<std::string, std::string>(Plate_Barcode, Well_Position, "_");
+    std::string tab_str = "\t";
+    std::vector<std::string> temp1 = combine_vectors<std::string, std::string>(Broad_Sample, mg_per_ml, tab_str);
+    std::vector<std::string> temp2 = combine_vectors<std::string, std::string>(temp1, mmoles_per_liter, tab_str);
+    compound_map = make_map(Plate_Barcode_Well_Position, temp2);
+
+   
 }
 
 void combine_lanes::get_count_file_map() {
@@ -187,7 +211,20 @@ std::string combine_lanes::get_mapped_str(std::string key_str) {
     std::string count_str = parts[4];
     int i5_plate = i5_key_to_plate[i5_id];
     int i5_quadrant = i5_key_to_quadrant[i5_id];
-    mapped_str = key_str + "\t" + std::to_string(i5_plate) + "\t" + std::to_string(i5_quadrant);
+    std::string plate_pool = std::to_string(i5_plate) + "_" + lane_str;
+    std::string plate_barcode = plate_bcs_map[plate_pool];
+    //std::cout << "plate_pool: " << plate_pool << "\n";
+    std::cout << "plate_barcode: " << plate_barcode << "\n";
+    std::string i7_key_quadrant = i7_id + "_" + std::to_string(i5_quadrant);
+    //std::cout << "i7_key_quadrant: " << i7_key_quadrant << "\n";
+    std::string row_column = i7_map[i7_key_quadrant];
+    //std::cout << "row_column: " << row_column << "\n";
+    std::string barcode_wellpos = plate_barcode + "_" + row_column;
+    std::string compound_info = compound_map[barcode_wellpos]; 
+    //std::cout << "compound_info: " << compound_info << "\n";
+    
+    mapped_str = key_str + "\t" + std::to_string(i5_plate) + "\t" + std::to_string(i5_quadrant) + "\t" +
+        plate_barcode + "\t" + row_column + "\t" + compound_info;
     return mapped_str;
 }
 
@@ -320,7 +357,7 @@ int main(int argc, char* argv[]) {
     } 
     cml.get_count_file_map();
     cml.load_maps();
-    //cml.join_lanes();
+    cml.join_lanes();
     cml.print_some_maps();
  
          
